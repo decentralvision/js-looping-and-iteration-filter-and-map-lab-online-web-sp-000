7289d7518511b4c050af51ed6eb6c54e672fc414
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 20:39:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  drivers.map{ function(driver) }
+  drivers.map{ function(driver) { return driver.revenue >= revenue }}
 }
