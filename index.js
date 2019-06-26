e507c32c4b5d7c792c3280e58db0ebfb177dada7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 21:29:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function exactMatch(drivers, specs) {
 }
 
 function exactMatchToList(drivers, specs) {
-  return exactMatch(drivers, specs).map( function(driver) {driver.name})
+  return exactMatch(drivers, specs).map( function(driver) { return driver.name})
 }
