ffafef4ca25ba6ef255fee4bed52714862c75ed4
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 21:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function driverNamesWithRevenueOver (drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue).map(function(driver) {return driver.name} )
 }
 
-
 function exactMatch(drivers, specs) {
   desiredKey = Object.keys(specs)[0]
   return drivers.filter(function(driver) {return driver[`${desiredKey}`] === specs[`${desiredKey}`] })
+}
+
+function exactMatchToList(drivers, specs) {
+  exactMatch(drivers, specs).map( function(driver) {driver.name})
 }
