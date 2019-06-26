e8b10c08ac79c821576ffbbb173b8e33ab2d6da5
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 20:53:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function driversWithRevenueOver (drivers, revenue) {
   return drivers.filter{ function(driver) { return driver.revenue > revenue }}
 }
+
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
